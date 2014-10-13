26f96c4e2f1438b4eabeb33038794ad911095cbf
dennis77pr <dennis77pr@gmail.com>
2014-10-12 21:18:10 -0700

    Adding test jade file

--- docker/node_server/start.sh
+++ docker/node_server/start.sh
@@ -1,11 +1,15 @@
 cd /tmp
 
+echo "removing old code"
 # try to remove the repo if it already exists
 rm -rf dockerNodeApp; true
 
+echo "getting new code from git."
 git clone https://github.com/dennis77pr/dockerNodeApp.git
 
 cd dockerNodeApp
+
+git pull -f
 
 npm install
 
